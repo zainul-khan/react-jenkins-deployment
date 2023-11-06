eaf1103c24164dc52910770fd4b2a0b6b2b9060e
zainul <kzainul22@gmail.com>
2023-11-07 01:01:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{textAlign: 'center'}}>Yo zainul bro</h1>
+    </div>
+  );
+}
+
+export default App;
